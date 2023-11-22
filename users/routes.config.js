63e008cd92deff3ec72227b86463d45fb069d395
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2023-11-22 20:32:06 +0300

    creating a permission component

--- users/routes.config.js
+++ users/routes.config.js
@@ -1,6 +1,25 @@
-app.post('/users',[
+app.post('/users', [
     UsersController.insert
 ]);
+app.get('/user', [
+    ValidationMiddlewar.validJWTNeeded,
+    PremissionMiddlewar.minimumPremissionLevelRequired(PAID),
+    UsersController.list
+]);
 app.get('/users/:userId', [
+    ValidationMiddlewar.validJWTNeeded,
+    PremissionMiddlewar.minimumPremissionLevelRequired(FREE),
+    PremissionMiddlewar.onlySameUserOrAdminCanDoThisAction,
     UsersController.getById
+]);
+app.patch('/useer/:userId', [
+    ValidationMiddlewar.validJWTNeeded,
+    PremissionMiddlewar.minimumPremissionLevelRequired(FREE),
+    PremissionMiddlewar.onlySameUserOrAdminCanDoThisAction,
+    UsersController.PatchById
+]);
+app.delete('/useer/:userId', [
+    ValidationMiddlewar.validJWTNeeded,
+    PremissionMiddlewar.minimumPremissionLevelRequired(ADMIN),
+    UsersController.removeById
 ]);
